ba937e2c350a8bbcfb9b32c6ee8750359b0cb3e1
Ken <ken.valverde@yahoo.com>
2021-08-10 15:55:47 -0500

    Create a bar graph to visualize the most popular repositories.

---
+++ python_repos_visual.py
@@ -0,0 +1,41 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Make an api call and store the response.
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Process results.
+response_dictionary = r.json()
+repository_dictionaries = response_dictionary['items']
+repository_names, stars = [], []
+for repository_dictionary in repository_dictionaries:
+    repository_names.append(repository_dictionary['name'])
+    stars.append(repository_dictionary['stargazers_count'])
+
+# Make visualization.
+data = [{
+    'type': 'bar',
+    'x': repository_names,
+    'y': stars,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on Github',
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Stars'},
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repost.html')
+
+
+
+
+
+
+
